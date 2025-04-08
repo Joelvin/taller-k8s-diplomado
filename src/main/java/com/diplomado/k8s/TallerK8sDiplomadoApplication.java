6dac22b18375349c901e5d92d798664cf1d88f54
JOELVIN ENRIQUE CHIRINO URBANO <e_jechirino@bancoppel.com>
2025-04-07 23:03:45 -0500

    Add source source ms code

---
+++ src/main/java/com/diplomado/k8s/TallerK8sDiplomadoApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerK8sDiplomadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerK8sDiplomadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomado/k8s/TallerK8sDiplomadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomado.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerK8sDiplomadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
